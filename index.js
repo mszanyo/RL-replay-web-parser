c98c2c9c5e3bdda6d059b30ac9230b1c486eac14
Mark Szanyo <mark@markszanyo.com>
2022-05-05 14:20:22 +0200

    Change headers allowed

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const app = express();
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization, Auth-Token, auth-token'
+	);
 	next();
 });
 
